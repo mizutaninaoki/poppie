29e09285ced44526b0b5642b3dbf903d9bb5ea62
mizutaninaoki <applewizardplanet@gmail.com>
2022-06-11 19:42:05 +0900

    delete comment

--- app/schema/types/plan_type.py
+++ app/schema/types/plan_type.py
@@ -3,9 +3,6 @@ from app.models.plan import Plan
 
 
 class PlanType(DjangoObjectType):
-    # 返り値をclass単位ではない値を返したい時は以下のようにする
-    # full_name = String()
-    # https://docs.graphene-python.org/en/latest/types/objecttypes/
     class Meta:
         model = Plan
         fields = ("id", "name", "fee")
