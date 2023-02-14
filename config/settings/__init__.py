4a8f98a55bb004918a410ee94bd762940b1a54a0
mizutaninaoki <applewizardplanet@gmail.com>
2023-02-14 15:45:49 +0900

    change

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -2,6 +2,7 @@ from .base import *
 
 env_name = os.environ.get("ENV_NAME")
 
+# 環境変数ENV_NAMEによって、ロードする設定ファイルを変更
 if env_name == "production":
     from .production import *
 elif env_name == "test":
