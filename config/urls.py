34e41aa9e575f10282758d7198ea48991ec4855e
mizutaninaoki <applewizardplanet@gmail.com>
2023-02-14 13:21:49 +0900

    change file name

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from django.conf.urls.static import static
-from .settings import base
+from .settings import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
-if base.DEBUG:
-    urlpatterns += static(base.MEDIA_URL, document_root=base.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
