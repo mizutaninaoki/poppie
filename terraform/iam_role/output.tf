1a1c74c2f9a810eb24531af0491c63e07bda45d0
mizutaninaoki <applewizardplanet@gmail.com>
2023-02-25 07:15:39 +0900

    delete comment

--- terraform/iam_role/output.tf
+++ terraform/iam_role/output.tf
@@ -1,7 +1,3 @@
-# output "aws_iam_role_ecs_task_execution" {
-#   value = aws_iam_role.ecs_task_execution
-# }
-
 # タスク実行ロール
 output "aws_iam_ecs_task_execution_role" {
   value = aws_iam_role.ecs_task_execution_role
