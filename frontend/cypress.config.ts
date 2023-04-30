8efdafe3232717259aa3c966e1e920711cf1c659
mizutaninaoki <applewizardplanet@gmail.com>
2023-05-01 01:22:46 +0900

    up

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
     // 以下にインテグレーションテストを走らせる前に設定することを書く
     // https://docs.cypress.io/guides/references/configuration#e2e
     // https://qiita.com/odennkunn/items/bd643f5aa1cd0e6930f0
-    setupNodeEvents(on, config) {
-      let envFile = config.env.CYPRESS_ENV === 'dev' ? '.e2e.env' : '.env.production';
-      require('dotenv').config({ path: envFile });
-    },
+    // setupNodeEvents(on, config) {
+    //   let envFile = config.env.CYPRESS_ENV === 'dev' ? '.e2e.env' : '.env.production';
+    //   require('dotenv').config({ path: envFile });
+    // },
   },
 });
