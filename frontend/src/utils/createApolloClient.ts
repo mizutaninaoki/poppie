25ea8a576736c2038998ea0da9e3f8e8aacb0e3d
mizutaninaoki <applewizardplanet@gmail.com>
2023-05-01 00:51:06 +0900

    try

--- frontend/src/utils/createApolloClient.ts
+++ frontend/src/utils/createApolloClient.ts
@@ -16,9 +16,12 @@ const REFRESH_TOKEN = `
 
 export function createApolloClient(): ApolloClient<NormalizedCacheObject> {
   // 本番環境の場合はnginxのポート番号を指定する
-  const scheme = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_SCHEME : "http";
-  const port = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_NGINX_PORT : ":8000";
-  const host = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_POPPIE_HOST : "localhost";
+  // const scheme = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_SCHEME : "http";
+  // const port = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_NGINX_PORT : ":8000";
+  // const host = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_POPPIE_HOST : "localhost";
+  const scheme = "http";
+  const port = ":8000";
+  const host = "localhost";
   const uri = `${scheme}://${host}${port}/graphql`;
   // const uri = `http://localhost:8000/graphql`;
 
